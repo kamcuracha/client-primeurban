55c5dda19c12a2b09354440109c356302dba167f
Kam Curacha <kamcuracha@yahoo.com>
2017-04-17 11:42:17 +0800

    #8 Contact page and other css fixes

--- wp-content/themes/lmtheme/footer.php
+++ wp-content/themes/lmtheme/footer.php
@@ -1,7 +1,7 @@
 <footer id="prefooter" class="section prefooter-section animatedParent">
   <div class="container">
     <div class="footer-logo animated fadeInUpShort" data-id="1">
-      <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-prime-urban.png" alt=""></a>
+      <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-prime-urban.png" alt=""></a>
     </div>
     <div class="footer-social animated fadeInUpShort" data-id="2">
       <?php if( have_rows('links', 'option') ): ?>
@@ -16,8 +16,8 @@
 
 <footer class="section footer-section">
   <div class="container">
-    <div class="t-footer-left">&copy Urban Prime <?php echo date("Y") ?>. All rights reserved.</div>
-    <div class="t-footer-right">Website by <a href="#">Light Media</a></div>
+    <div class="t-footer-left">&copy Prime Urban <?php echo date("Y") ?>. All rights reserved.</div>
+    <div class="t-footer-right">Website by <a target="_blank" href="https://www.lightmedia.com.au/">Light Media</a></div>
   </div>
 </footer>
 
--- wp-content/themes/lmtheme/page-templates/contact-page.php
+++ wp-content/themes/lmtheme/page-templates/contact-page.php
@@ -31,54 +31,58 @@ get_header(); // Loads the header.php template. ?>
 
     <div id="contact-us" class="section section-contact-us">
   <div class="container">
-    <div class="row animatedParent">
-      <div class="col-sm-6 animated fadeInLeft">
-        <h2>Contact Form</h2>
-        <?php echo do_shortcode('[formidable id=2]'); ?>
-      </div>
-      <div class="col-sm-6 animated fadeInRight">
-        <div class="contact-details">
-          <h2>Contact Details</h2>
-          <ul class="contact">
+    <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+            <div class="row animatedParent">
+                <div class="col-sm-6 animated fadeInLeft">
+                    <h2>Contact Form</h2>
+                    <?php echo do_shortcode('[formidable id=2]'); ?>
+                </div>
+                <div class="col-sm-6 animated fadeInRight">
+                    <div class="contact-details">
+                        <h2>Contact Details</h2>
+                        <ul class="contact">
 
-            <?php if ( get_field('address') ): ?>
-            <li><i class="fa fa-home" aria-hidden="true"></i> <span><?php the_field('address'); ?></span></li>
-            <?php else: ?>
-            <li><i class="fa fa-home" aria-hidden="true"></i> <span>Suite 522, St Kilda Road Towers<br>
+                            <?php if ( get_field('address') ): ?>
+                                <li><i class="fa fa-home" aria-hidden="true"></i> <span><?php the_field('address'); ?></span></li>
+                            <?php else: ?>
+                                <li><i class="fa fa-home" aria-hidden="true"></i> <span>Suite 522, St Kilda Road Towers<br>
           1 Queens Road, Melbourne<br>
           Victoria 3004</span></li>
-            <?php endif; ?>
+                            <?php endif; ?>
 
-            <?php if ( get_field('phone') ): ?>
-            <li><i class="fa fa-phone" aria-hidden="true"></i> <span><?php the_field('phone'); ?></span></li>
-            <?php endif; ?>
+                            <?php if ( get_field('phone') ): ?>
+                                <li><i class="fa fa-phone" aria-hidden="true"></i> <span><?php the_field('phone'); ?></span></li>
+                            <?php endif; ?>
 
-            <?php if ( get_field('email') ): ?>
-            <li><i class="fa fa-envelope" aria-hidden="true"></i> <span><?php the_field('email'); ?></span></li>
-            <?php endif; ?>
+                            <?php if ( get_field('email') ): ?>
+                                <li><i class="fa fa-envelope" aria-hidden="true"></i> <span><?php the_field('email'); ?></span></li>
+                            <?php endif; ?>
 
-            <?php if ( get_field('address') ): ?>
-            <li>
-              <div class="map-holder"> 
-                <div id="map-canvas" data-title="<?php $address = get_field('address'); $address = preg_replace( "/\r|\n/", "", $address); echo strip_tags($address); ?>" style="width:420px;height:275px;"></div> 
-              </div>
-            </li>
-            <?php else: ?>
-            <li>
-              <div class="map-holder"> 
-                <div id="map-canvas" data-lat="-37.835768" data-lng="144.973876" data-title="Suite 522, St Kilda Road Towers 1 Queens Road, Melbourne Victoria 3004" style="width:420px;height:275px;"></div> 
-              </div>
-            </li>
-            <?php endif; ?>
+                            <?php if ( get_field('address') ): ?>
+                                <li>
+                                    <div class="map-holder">
+                                        <div id="map-canvas" data-title="<?php $address = get_field('address'); $address = preg_replace( "/\r|\n/", "", $address); echo strip_tags($address); ?>" style="width:420px;height:275px;"></div>
+                                    </div>
+                                </li>
+                            <?php else: ?>
+                                <li>
+                                    <div class="map-holder">
+                                        <div id="map-canvas" data-lat="-37.835768" data-lng="144.973876" data-title="Suite 522, St Kilda Road Towers 1 Queens Road, Melbourne Victoria 3004" style="width:420px;height:275px;"></div>
+                                    </div>
+                                </li>
+                            <?php endif; ?>
 
-            <?php if ( get_field('link') ): ?>
-            <li><i class="fa fa-map-marker" aria-hidden="true"></i> <a href="<?php the_field('link'); ?>" target="_blank"><span>Get Directions</span></a></li>
-            <?php else: ?>
-            <li><i class="fa fa-map-marker" aria-hidden="true"></i> <a href="//goo.gl/kia3R2" target="_blank"><span>Get Directions</span></a></li>
-            <?php endif; ?>
-          </ul>
+                            <?php if ( get_field('link') ): ?>
+                                <li><i class="fa fa-map-marker" aria-hidden="true"></i> <a href="<?php the_field('link'); ?>" target="_blank"><span>Get Directions</span></a></li>
+                            <?php else: ?>
+                                <li><i class="fa fa-map-marker" aria-hidden="true"></i> <a href="//goo.gl/kia3R2" target="_blank"><span>Get Directions</span></a></li>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   </div>
 </div>
